39ceef51ff5ce492f405d768beac2b25b647a794
Michel Farah <michelfarahh7@gmail.com>
2023-10-04 02:22:04 -0400

    finished MinimalSubscriber class python

--- src/py_subscriber/py_subscriber/py_subscriber.py
+++ src/py_subscriber/py_subscriber/py_subscriber.py
@@ -2,3 +2,17 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription 
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard the even number: "%s"' % msg.data)
