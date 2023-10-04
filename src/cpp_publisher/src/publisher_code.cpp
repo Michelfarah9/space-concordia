dd9be9032b1169c4a97b1687155561602258075f
Michel Farah <michelfarahh7@gmail.com>
2023-10-04 02:05:30 -0400

    finished the main of publisher

--- src/cpp_publisher/src/publisher_code.cpp
+++ src/cpp_publisher/src/publisher_code.cpp
@@ -38,3 +38,11 @@ public:
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
 };
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
